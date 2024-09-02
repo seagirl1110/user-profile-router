7b7d9511667e152d5634d751d29a4fbb7ab0903f
Marina Elizarova <seagirl1110@gmail.com>
2024-09-02 14:15:35 +0300

    feat: setup routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import UserProfile from './pages/UserProfile';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
